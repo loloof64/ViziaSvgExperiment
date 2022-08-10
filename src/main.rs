e968ffed3d4ef3bbb5ac8da6867a39111c5737ad
loloof64 <laurent.bernabe@gmail.com>
2022-08-11 01:19:17 +0200

    Set up all pieces back

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,25 @@ impl View for SvgZone {
 
 fn main() {
     Application::new(|cx| {
-        SvgZone::new(cx, include_bytes!("resources/Chess_rdt45.svg")).size(Pixels(700.0));
+        VStack::new(cx, |cx| {
+            HStack::new(cx, |cx| {
+                SvgZone::new(cx, include_bytes!("resources/Chess_plt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_nlt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_blt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_rlt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_qlt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_klt45.svg")).size(Pixels(100.0));
+            });
+
+            HStack::new(cx, |cx| {
+                SvgZone::new(cx, include_bytes!("resources/Chess_pdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_ndt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_bdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_rdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_qdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_kdt45.svg")).size(Pixels(100.0));
+            });
+        });
     })
     .title("SVG")
     .inner_size((800, 800))
