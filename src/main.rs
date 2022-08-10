2131486cc63c31d1ddd2dde4951a3fcb490e636c
loloof64 <laurent.bernabe@gmail.com>
2022-08-10 15:30:12 +0200

    Implemented a running example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use vizia::prelude::*;
+
+#[derive(Lens)]
+struct AppData {}
+
+pub enum AppEvent {
+    Hello,
+}
+
+impl Model for AppData {
+    fn event(&mut self, _: &mut EventContext, event: &mut Event) {
+        event.map(|app_event, _| match app_event {
+            AppEvent::Hello => {
+                println!("Hello !");
+            }
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Application::new(|cx| {
+        AppData{}.build(cx);
+
+        Button::new(
+            cx,
+            |cx| cx.emit(AppEvent::Hello),
+            |cx| Label::new(cx, "Say hello"),
+        );
+    })
+    .title("Vizia svg experiment")
+    .inner_size((400, 100))
+    .run()
 }
