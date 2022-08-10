7a98cbf2d1d9bdcd468defec8fc006f96f698890
loloof64 <laurent.bernabe@gmail.com>
2022-08-10 23:33:29 +0200

    Replaced knights vectors with the original ones

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ A simple experiment
 ## Credits
 
 Chess vectors have been imported from [Wikimedia Commons](https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces) and designed by CBurnett.
-White knight and black knight have been slightly modified by loloof64.
--- src/main.rs
+++ src/main.rs
@@ -81,26 +81,26 @@ fn main() {
     Application::new(|cx| {
         VStack::new(cx, |cx| {
             HStack::new(cx, |cx| {
-                SvgZone::new(cx, include_bytes!("resources/Chess_plt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_nlt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_blt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_rlt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_qlt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_klt45.svg")).size(Pixels(50.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_plt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_nlt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_blt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_rlt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_qlt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_klt45.svg")).size(Pixels(100.0));
             });
 
             HStack::new(cx, |cx| {
-                SvgZone::new(cx, include_bytes!("resources/Chess_pdt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_ndt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_bdt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_rdt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_qdt45.svg")).size(Pixels(50.0));
-                SvgZone::new(cx, include_bytes!("resources/Chess_kdt45.svg")).size(Pixels(50.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_pdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_ndt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_bdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_rdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_qdt45.svg")).size(Pixels(100.0));
+                SvgZone::new(cx, include_bytes!("resources/Chess_kdt45.svg")).size(Pixels(100.0));
             });
         });
     })
     .title("SVG")
-    .inner_size((450, 450))
+    .inner_size((800, 800))
     .run()
 }
 
